d6dfd364a289969db45bca2a5ad5c4277d067ba7
SimonsUnikaNamn <simonanders@gmail.com>
2022-01-13 16:58:07 +0100

    feat: 3rd task together with motivation

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Rust exercises
 
-This package implements 3 different rust exercies. Run `cargo test` to run the corresponding test for each exercise.
+This package implements 3 different rust exercies. Run `cargo test` to run the corresponding test for the first two exercises.
+The 3rd task can be run with `cargo run`
+
+## On 3rd task
+I was unable to get this to work within a reasonable time frame and have decided to not implement it fully.
+
+I understand that I should take the binary array and remove `13u8` from each value and the get the encoded string.
+However using the standard `write` method I am unable to manipulate the actual bytes. As per the documentation
+
+(The write method will attempt to write some data into the object, returning how many bytes were successfully written.)[https://doc.rust-lang.org/std/io/trait.Write.html]
+
+I understand that I should have used the `BufWriter` in some capacity since it looks to allow for byte manipulation. According to the docs it looked like I would be able to wrap my `self.content` inside a `BufWriter` since it implemented the `Write` method but I had no such sucess.
+
+```
+let mut buffer = BufWriter::new(self.content);
+   |                                         ^^^^^^^^^^^^ move occurs because `self.content` has type `T`, which does not implement the `Copy` trait
+```
+
+After some testing and looking online I have decided to not continue with the final task of decoding the message. I was able to copy the bytes from one array to another, that is how far I got.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-}
+use memoize::memoize;
+use std::io::Write;
+
 
 /// fizzbuzz
 /// ---
@@ -24,4 +24,84 @@ fn fizzbuzz_gives_correct_result_for_a_the_first_15_values() {
     }
 }
 
+// I would have wanted to avoid using a library for memoizing and instead used a static hashmap<usize,usize> 
+// that I would update continously with the values returned from the fib. 
+// But it seems like that is a bit more involved in rust than I am familiar with so I will use the library instead
+// 
+/// fib
+/// ---
+/// fib(0) = 0
+/// fib(1) = 1
+/// fib(n) = fib(n-1) + fib(n-2) for all n >= 0
+#[memoize]
+pub fn fib(n: usize) -> usize {
+    match n {
+        0 => return 0,
+        1 => return 1,
+        _ => return fib(n-1) + fib(n-2)
+    }
+}
 
+// Found an example of the first 21 Fibonacci numbers here https://en.wikipedia.org/wiki/Fibonacci_number
+#[test]
+fn fib_gives_correct_result_a_few_values() {
+    assert_eq!(fib(0), 0);
+    assert_eq!(fib(1), 1);
+    assert_eq!(fib(2), 1);
+    assert_eq!(fib(3), 2);
+    assert_eq!(fib(10), 55);
+    assert_eq!(fib(19), 4181);
+}
+
+
+struct Rot13Writer<T>
+where
+    T: Write,
+{
+    content: T
+}
+
+impl<T> Rot13Writer<T>
+where
+    T: Write,
+{
+    pub fn new(inner: T) -> Self {
+        return Rot13Writer { content: inner}
+    }
+}
+
+impl<T> Write for Rot13Writer<T>
+where
+    T: Write,
+{
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let mut pos = 0;
+        while pos < buf.len() {
+            let bytes_written = self.content.write(&buf[pos..])?;
+            pos += bytes_written;
+        }
+
+        Ok(pos)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
+
+fn main() {
+    let mut content = Vec::<u8>::default();
+    let mut buff = Rot13Writer::new(&mut content);
+    buff.write(b"Lbh penpxrq zl fhcre qvssvphyg pbqvat punyyratr... pbqr vf ddommNst")
+        .unwrap();
+
+    println!(
+        "result: {:?}",
+        content.iter().map(|x| *x as char).collect::<String>()
+    );
+}
+
+#[test]
+fn test_rot13() {
+    main()
+}
